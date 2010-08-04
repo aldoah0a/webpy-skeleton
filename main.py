c7bbfe5aa87af7fa0d541895c6fd1d56bae68590
Abtin Forouzandeh <abtinf@gmail.com>
2010-08-04 13:42:13 -0700

    Added support for custom error messages.

--- main.py
+++ main.py
@@ -13,7 +13,16 @@ class index:
   def GET(self):
     return render.index()
 
+def notfound():
+  return web.notfound(render.error_404())
+
+def internalerror():
+  return web.internalerror(render.error_500())
+
 app = web.application(urls, globals())
+
+app.notfound = notfound
+app.internalerror = internalerror
 
 if __name__ == "__main__":
   app.run()
